127e1a27081db87d30da7dda6e8722333f59fbfb
abhijith-lang <abhijith@elemnt8.ae>
2026-09-19 08:56:19 +0530

    Organize homepage styles by component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
 import "../styles/tailwind.css";
 import "../styles/lenis.css";
-import "../styles/adure/index.css";
+import "../styles/global/common.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
